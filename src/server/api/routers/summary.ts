bcb9a95fd1b3322ee823a0e001bf1c119cb95b74
swasthikshetty10 <swasthikshetty10902@gmail.com>
2023-04-15 22:34:55 +0530

    feat: meeting minutes

---
+++ src/server/api/routers/summary.ts
@@ -0,0 +1,85 @@
+import axios from "axios";
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
+
+import { Configuration, OpenAIApi } from "openai";
+import { z } from "zod";
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_API_SECRET,
+});
+const openai = new OpenAIApi(configuration);
+export const roomsRouter = createTRPCRouter({
+  getRoomSummary: protectedProcedure
+    .input(
+      z.object({
+        roomName: z.string(),
+      })
+    )
+    .query(async ({ input, ctx }) => {
+      const transcripts = await ctx.prisma.transcript.findMany({
+        where: {
+          Room: {
+            name: input.roomName,
+          },
+        },
+        include: {
+          User: true,
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      });
+      const chatLog = transcripts.map((transcript) => ({
+        speaker: transcript.User?.name,
+        utterance: transcript.transcription,
+        timestamp: transcript.createdAt.toISOString(),
+      }));
+      if (chatLog.length === 0) {
+        return null;
+      }
+
+      const apiKey = process.env.ONEAI_API_KEY;
+      console.log(chatLog);
+      try {
+        const config = {
+          method: "POST",
+          url: "https://api.oneai.com/api/v0/pipeline",
+          headers: {
+            "api-key": apiKey,
+            "Content-Type": "application/json",
+          },
+          data: {
+            input: chatLog,
+            input_type: "conversation",
+            content_type: "application/json",
+            output_type: "json",
+            multilingual: {
+              enabled: true,
+            },
+            steps: [
+              {
+                skill: "article-topics",
+              },
+              {
+                skill: "numbers",
+              },
+              {
+                skill: "names",
+              },
+              {
+                skill: "emotions",
+              },
+              {
+                skill: "summarize",
+              },
+            ],
+          },
+        };
+
+        const res = await axios.request(config);
+        console.log(res.status);
+        return res.data;
+      } catch (error) {
+        console.log(error);
+      }
+    }),
+});
